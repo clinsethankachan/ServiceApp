1d55c10368560d16ac7157f1bc6f11a9586335fe
Clinse Thankachan <clinzthankach@gmail.com>
2021-06-04 14:45:29 +0530

    Home page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserHomeComponent } from './user-home/user-home.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserHomeComponent
+    UserHomeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
